04c03366b323128ab18606fd3282adf537810add
TejuPhasale04 <phasaletejaswini@gmail.com>
2025-06-20 19:34:51 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+model = joblib.load("F:/Its Starting/1. House Price Prediction/Model/house_price_model.pkl")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+
+    binary_map = {'yes': 1, 'no': 0}
+
+    # Convert input to match model format (13 features)
+    input_data = [
+        float(data['area']),
+        int(data['bedrooms']),
+        int(data['bathrooms']),
+        int(data['stories']),
+        binary_map[data['mainroad']],
+        binary_map[data['guestroom']],
+        binary_map[data['basement']],
+        binary_map[data['hotwaterheating']],
+        binary_map[data['airconditioning']],
+        int(data['parking']),
+        binary_map[data['prefarea']],
+        1 if data['furnishingstatus'] == 'semi-furnished' else 0,
+        1 if data['furnishingstatus'] == 'unfurnished' else 0
+    ]
+
+    input_array = np.array([input_data])
+    prediction = model.predict(input_array)[0]
+
+    return jsonify({"predicted_price": prediction})
+
+if __name__ == '__main__':
+    app.run(debug=True)
