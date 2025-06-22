89d93704b711c3d9b87b2e2ae8dbb7da15b54c61
Tejaswini Phasale <119858988+TejuPhasale04@users.noreply.github.com>
2025-06-22 21:19:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import numpy as np
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -37,4 +39,6 @@ def predict():
     return jsonify({"predicted_price": prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port)
+
